3fe1bed8a2a1631c790e5a22deb3700bbb505736
박병훈 <leafloveu@naver.com>
2023-09-21 17:27:59 +0900

    bhoon

--- kr/ac/jbnu/se/tetris/Board.java
+++ kr/ac/jbnu/se/tetris/Board.java
@@ -18,7 +18,7 @@ public class Board extends JPanel implements ActionListener {
 	final int BoardHeight = 22; //게임 보드의 세로 칸 수
 
 	Timer timer; //게임의 속도를 조절하는 타이머
-	boolean isFallingFinished = false; //블럭이 떨어지는 것이 끝났는지를 나타내는 변수
+	boolean isFallingFinished = false; //블록이 떨어지는 것이 끝났는지를 나타내는 변수
 	boolean isStarted = false; //게임이 시작되었는지를 나타내는 변수
 	boolean isPaused = false; //게임이 일시정지되었는지를 나타내는 변수
 	int numLinesRemoved = 0; //제거된 줄의 수를 나타내는 변수
@@ -42,11 +42,11 @@ public class Board extends JPanel implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) { //타이머가 400ms마다 호출하는 메소드
-		if (isFallingFinished) { //블럭이 떨어지는 것이 끝났다면
+		if (isFallingFinished) { //블록이 떨어지는 것이 끝났다면
 			isFallingFinished = false; //끝났음을 나타내는 변수를 false로 설정
-			newPiece(); //새로운 블럭을 생성
+			newPiece(); //새로운 블록을 생성
 		} else { //떨어지는 것이 끝나지 않았다면
-			oneLineDown(); //블럭을 한 칸 아래로 이동
+			oneLineDown(); //블록을 한 칸 아래로 이동
 		}
 	}
 
@@ -59,7 +59,7 @@ public class Board extends JPanel implements ActionListener {
 	}
 
 	Tetrominoes shapeAt(int x, int y) {
-		return board[(y * BoardWidth) + x]; //게임 보드의 (x, y) 위치에 있는 블럭의 모양을 반환
+		return board[(y * BoardWidth) + x]; //게임 보드의 (x, y) 위치에 있는 블록의 모양을 반환
 	}
 
 	public void start() { //게임을 시작하는 메소드
@@ -67,11 +67,11 @@ public class Board extends JPanel implements ActionListener {
 			return; //메소드 종료
 
 		isStarted = true; //게임이 시작되었음을 나타내는 변수를 true로 설정
-		isFallingFinished = false; //블럭이 떨어지는 것이 끝났음을 나타내는 변수를 false로 설정
+		isFallingFinished = false; //블록이 떨어지는 것이 끝났음을 나타내는 변수를 false로 설정
 		numLinesRemoved = 0; //제거된 줄의 수를 0으로 설정
 		clearBoard(); //게임 보드를 초기화
 
-		newPiece(); //새로운 블럭을 생성
+		newPiece(); //새로운 블록을 생성
 		timer.start(); //타이머 시작
 	}
 
@@ -98,35 +98,35 @@ public class Board extends JPanel implements ActionListener {
 
 		for (int i = 0; i < BoardHeight; ++i) {
 			for (int j = 0; j < BoardWidth; ++j) { //게임 보드의 모든 칸에 대해
-				Tetrominoes shape = shapeAt(j, BoardHeight - i - 1); //게임 보드의 (j, BoardHeight - i - 1) 위치에 있는 블럭의 모양을 가져옴
-				if (shape != Tetrominoes.NoShape) //블럭의 모양이 NoShape(없음)이 아니라면
-					drawSquare(g, 0 + j * squareWidth(), boardTop + i * squareHeight(), shape); //블럭을 그림
+				Tetrominoes shape = shapeAt(j, BoardHeight - i - 1); //게임 보드의 (j, BoardHeight - i - 1) 위치에 있는 블록의 모양을 가져옴
+				if (shape != Tetrominoes.NoShape) //블록의 모양이 NoShape(없음)이 아니라면
+					drawSquare(g, 0 + j * squareWidth(), boardTop + i * squareHeight(), shape); //블록을 그림
 			}
 		}
 
-		if (curPiece.getShape() != Tetrominoes.NoShape) { //현재 블럭의 모양이 NoShape(없음)이 아니라면
-			for (int i = 0; i < 4; ++i) { //현재 블럭의 모든 칸에 대해
-				int x = curX + curPiece.x(i); //현재 블럭의 x좌표
-				int y = curY - curPiece.y(i); //현재 블럭의 y좌표
+		if (curPiece.getShape() != Tetrominoes.NoShape) { //현재 블록의 모양이 NoShape(없음)이 아니라면
+			for (int i = 0; i < 4; ++i) { //현재 블록의 모든 칸에 대해
+				int x = curX + curPiece.x(i); //현재 블록의 x좌표
+				int y = curY - curPiece.y(i); //현재 블록의 y좌표
 				drawSquare(g, 0 + x * squareWidth(), boardTop + (BoardHeight - y - 1) * squareHeight(), 
-						curPiece.getShape()); //현재 블럭을 그림
+						curPiece.getShape()); //현재 블록을 그림
 			}
 		}
 	}
 
-	private void dropDown() { //블럭을 한 칸 아래로 이동하는 메소드
-		int newY = curY; //현재 블럭의 y좌표를 저장
-		while (newY > 0) { //현재 블럭의 y좌표가 0보다 크다면 반복
-			if (!tryMove(curPiece, curX, newY - 1)) //블럭을 한 칸 아래로 이동할 수 없다면
+	private void dropDown() { //블록을 한 칸 아래로 이동하는 메소드
+		int newY = curY; //현재 블록의 y좌표를 저장
+		while (newY > 0) { //현재 블록의 y좌표가 0보다 크다면 반복
+			if (!tryMove(curPiece, curX, newY - 1)) //블록을 한 칸 아래로 이동할 수 없다면
 				break; //반복문 종료
-			--newY; //현재 블럭의 y좌표를 1만큼 감소
+			--newY; //현재 블록의 y좌표를 1만큼 감소
 		}
-		pieceDropped(); //블럭을 한 칸 아래로 이동
+		pieceDropped(); //블록을 한 칸 아래로 이동
 	}
 
-	private void oneLineDown() { //블럭을 한 칸 아래로 이동하는 메소드
-		if (!tryMove(curPiece, curX, curY - 1)) //블럭을 한 칸 아래로 이동할 수 없다면
-			pieceDropped(); //블럭을 한 칸 아래로 이동
+	private void oneLineDown() { //블록을 한 칸 아래로 이동하는 메소드
+		if (!tryMove(curPiece, curX, curY - 1)) //블록을 한 칸 아래로 이동할 수 없다면
+			pieceDropped(); //블록을 한 칸 아래로 이동
 	}
 
 	private void clearBoard() { //게임 보드를 초기화하는 메소드
@@ -134,26 +134,26 @@ public class Board extends JPanel implements ActionListener {
 			board[i] = Tetrominoes.NoShape; //칸의 모양을 NoShape(없음)으로 설정
 	}
 
-	private void pieceDropped() { //블럭을 한 칸 아래로 이동하는 메소드
-		for (int i = 0; i < 4; ++i) { //현재 블럭의 모든 칸에 대해
-			int x = curX + curPiece.x(i); //현재 블럭의 x좌표
-			int y = curY - curPiece.y(i); //현재 블럭의 y좌표
-			board[(y * BoardWidth) + x] = curPiece.getShape(); //게임 보드의 (x, y) 위치에 현재 블럭의 모양을 저장
+	private void pieceDropped() { //블록을 한 칸 아래로 이동하는 메소드
+		for (int i = 0; i < 4; ++i) { //현재 블록의 모든 칸에 대해
+			int x = curX + curPiece.x(i); //현재 블록의 x좌표
+			int y = curY - curPiece.y(i); //현재 블록의 y좌표
+			board[(y * BoardWidth) + x] = curPiece.getShape(); //게임 보드의 (x, y) 위치에 현재 블록의 모양을 저장
 		}
 
-		removeFullLines(); //블럭을 내리고 가득 찬 줄이 있으면 제거
+		removeFullLines(); //블록을 내리고 가득 찬 줄이 있으면 제거
 
-		if (!isFallingFinished) //블럭이 떨어지는 것이 끝나지 않았다면?
-			newPiece(); //새로운 블럭을 생성?
+		if (!isFallingFinished) //블록이 떨어지는 것이 끝나지 않았다면?
+			newPiece(); //새로운 블록을 생성?
 	}
 
-	private void newPiece() { //새로운 블럭을 생성하는 메소드
-		curPiece.setRandomShape(); //새로운 블럭의 모양을 랜덤으로 설정
-		curX = BoardWidth / 2 + 1; //새로운 블럭의 x좌표
-		curY = BoardHeight - 1 + curPiece.minY(); //새로운 블럭의 y좌표
+	private void newPiece() { //새로운 블록을 생성하는 메소드
+		curPiece.setRandomShape(); //새로운 블록의 모양을 랜덤으로 설정
+		curX = BoardWidth / 2 + 1; //새로운 블록의 x좌표
+		curY = BoardHeight - 1 + curPiece.minY(); //새로운 블록의 y좌표
 
-		if (!tryMove(curPiece, curX, curY)) { //새로운 위치로 블럭을 이동할 수 없다면
-			curPiece.setShape(Tetrominoes.NoShape); //현재 블럭의 모양을 NoShape(없음)으로 설정
+		if (!tryMove(curPiece, curX, curY)) { //새로운 위치로 블록을 이동할 수 없다면
+			curPiece.setShape(Tetrominoes.NoShape); //현재 블록의 모양을 NoShape(없음)으로 설정
 			timer.stop(); //타이머 정지
 			isStarted = false; //게임이 시작되었음을 나타내는 변수를 false로 설정
 			statusbar.setText("game over"); //게임의 상태를 나타내는 레이블에 "game over"를 출력
@@ -161,23 +161,23 @@ public class Board extends JPanel implements ActionListener {
 	}
 
 	private boolean tryMove(Shape newPiece, int newX, int newY) { //새로운 위치(newX, newY)로 블록을 이동하려고 시도하는 메소드
-		for (int i = 0; i < 4; ++i) { //새로운 블럭의 모든 칸에 대해
-			int x = newX + newPiece.x(i); //새로운 블럭의 x좌표
-			int y = newY - newPiece.y(i); //새로운 블럭의 y좌표
-			if (x < 0 || x >= BoardWidth || y < 0 || y >= BoardHeight) //새로운 블럭이 게임 보드의 범위를 벗어난다면
+		for (int i = 0; i < 4; ++i) { //새로운 블록의 모든 칸에 대해
+			int x = newX + newPiece.x(i); //새로운 블록의 x좌표
+			int y = newY - newPiece.y(i); //새로운 블록의 y좌표
+			if (x < 0 || x >= BoardWidth || y < 0 || y >= BoardHeight) //새로운 블록이 게임 보드의 범위를 벗어난다면
 				return false; //false 반환
-			if (shapeAt(x, y) != Tetrominoes.NoShape) //새로운 블럭이 게임 보드의 다른 블럭과 겹친다면
+			if (shapeAt(x, y) != Tetrominoes.NoShape) //새로운 블록이 게임 보드의 다른 블록과 겹친다면
 				return false; //false 반환
 		}
 
-		curPiece = newPiece; //새로운 블럭을 현재 블럭으로 설정
-		curX = newX; //현재 블럭의 x좌표를 newX로 설정
-		curY = newY; //현재 블럭의 y좌표를 newY로 설정
+		curPiece = newPiece; //새로운 블록을 현재 블록으로 설정
+		curX = newX; //현재 블록의 x좌표를 newX로 설정
+		curY = newY; //현재 블록의 y좌표를 newY로 설정
 		repaint(); //게임 보드를 다시 그림
 		return true; //true 반환
 	}
 
-	private void removeFullLines() { //블럭을 내리고 가득 찬 줄이 있으면 제거하는 메소드
+	private void removeFullLines() { //블록을 내리고 가득 찬 줄이 있으면 제거하는 메소드
 		int numFullLines = 0; //가득 찬 줄의 수를 나타내는 변수
 
 		for (int i = BoardHeight - 1; i >= 0; --i) { //게임 보드의 가장 아래쪽 줄부터 검사
@@ -202,35 +202,35 @@ public class Board extends JPanel implements ActionListener {
 		if (numFullLines > 0) { //가득 찬 줄이 있다면
 			numLinesRemoved += numFullLines; //제거된 줄의 수를 증가
 			statusbar.setText(String.valueOf(numLinesRemoved)); //게임의 상태를 나타내는 레이블에 제거된 줄의 수를 출력
-			isFallingFinished = true; //블럭이 떨어지는 것이 끝났음을 나타내는 변수를 true로 설정
-			curPiece.setShape(Tetrominoes.NoShape); //현재 블럭의 모양을 NoShape(없음)으로 설정
+			isFallingFinished = true; //블록이 떨어지는 것이 끝났음을 나타내는 변수를 true로 설정
+			curPiece.setShape(Tetrominoes.NoShape); //현재 블록의 모양을 NoShape(없음)으로 설정
 			repaint(); //게임 보드를 다시 그림
 		}
 	}
 
-	private void drawSquare(Graphics g, int x, int y, Tetrominoes shape) { //x, y는 블럭 왼쪽 상단의 좌표, shape는 블럭의 모양
+	private void drawSquare(Graphics g, int x, int y, Tetrominoes shape) { //x, y는 블록 왼쪽 상단의 좌표, shape는 블록의 모양
 		Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102), new Color(102, 204, 102), //색상 배열(RGB)
 				new Color(102, 102, 204), new Color(204, 204, 102), new Color(204, 102, 204), new Color(102, 204, 204),
 				new Color(218, 170, 0) };
 
-		Color color = colors[shape.ordinal()]; //블럭의 모양에 따라 색상을 정함
+		Color color = colors[shape.ordinal()]; //블록의 모양에 따라 색상을 정함
 
-		g.setColor(color); //블럭의 색상을 정함
-		g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2); //블럭 내부를 그림
+		g.setColor(color); //블록의 색상을 정함
+		g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2); //블록 내부를 그림
 
 		g.setColor(color.brighter()); //밝은 색상으로 설정
-		g.drawLine(x, y + squareHeight() - 1, x, y); //블럭의 왼쪽에 밝은 선을 그림(경계선)
-		g.drawLine(x, y, x + squareWidth() - 1, y); //블럭의 위쪽에 밝은 선을 그림
+		g.drawLine(x, y + squareHeight() - 1, x, y); //블록의 왼쪽에 밝은 선을 그림(경계선)
+		g.drawLine(x, y, x + squareWidth() - 1, y); //블록의 위쪽에 밝은 선을 그림
 
 		g.setColor(color.darker()); //어두운 색상으로 설정
-		g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1); //블럭의 아래쪽에 어두운 선을 그림(그림자)
-		g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1); //블럭의 오른쪽에 어두운 선을 그림
+		g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1); //블록의 아래쪽에 어두운 선을 그림(그림자)
+		g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1); //블록의 오른쪽에 어두운 선을 그림
 	}
 
 	class TAdapter extends KeyAdapter { //키보드 입력을 받는 클래스
 		public void keyPressed(KeyEvent e) {
 
-			if (!isStarted || curPiece.getShape() == Tetrominoes.NoShape) { //게임이 시작되지 않았거나 현재 블럭의 모양이 NoShape(없음)이라면
+			if (!isStarted || curPiece.getShape() == Tetrominoes.NoShape) { //게임이 시작되지 않았거나 현재 블록의 모양이 NoShape(없음)이라면
 				return; //메소드 종료
 			}
 
--- kr/ac/jbnu/se/tetris/Shape.java
+++ kr/ac/jbnu/se/tetris/Shape.java
@@ -2,20 +2,20 @@ package kr.ac.jbnu.se.tetris;
 
 import java.util.Random;
 
-public class Shape { //블럭의 모양을 나타내는 클래스
+public class Shape { //블록의 모양을 나타내는 클래스
 
 	private Tetrominoes pieceShape;
 	private int coords[][];
 	private int[][][] coordsTable;
 
 	public Shape() { //생성자
-		coords = new int[4][2]; //블럭의 좌표를 나타내는 배열 생성
-		setShape(Tetrominoes.NoShape); //블럭의 모양을 NoShape(없음)으로 설정
+		coords = new int[4][2]; //블록의 좌표를 나타내는 배열 생성
+		setShape(Tetrominoes.NoShape); //블록의 모양을 NoShape(없음)으로 설정
 	}
 
-	public void setShape(Tetrominoes shape) { //블럭의 모양을 설정하는 메소드
+	public void setShape(Tetrominoes shape) { //블록의 모양을 설정하는 메소드
 
-		//블럭의 상대적인 좌표를 나타내는 배열(NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape)
+		//블록의 상대적인 좌표를 나타내는 배열(NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape)
 		coordsTable = new int[][][] { { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } },
 				{ { 0, -1 }, { 0, 0 }, { -1, 0 }, { -1, 1 } }, { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } },
 				{ { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } }, { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } },
@@ -32,34 +32,34 @@ public class Shape { //블럭의 모양을 나타내는 클래스
 		pieceShape = shape; //현재 블록(pieceShape)를 매개변수 shape로 업데이트
 	}
 
-	private void setX(int index, int x) { //블럭의 x좌표를 설정하는 메소드
+	private void setX(int index, int x) { //블록의 x좌표를 설정하는 메소드
 		coords[index][0] = x; //coords 배열의 index번째 칸의 x좌표를 x로 설정
 	}
 
-	private void setY(int index, int y) { //블럭의 y좌표를 설정하는 메소드
+	private void setY(int index, int y) { //블록의 y좌표를 설정하는 메소드
 		coords[index][1] = y;
 	}
 
-	public int x(int index) { //블럭의 x좌표를 반환하는 메소드
+	public int x(int index) { //블록의 x좌표를 반환하는 메소드
 		return coords[index][0];
 	}
 
-	public int y(int index) { //블럭의 y좌표를 반환하는 메소드
+	public int y(int index) { //블록의 y좌표를 반환하는 메소드
 		return coords[index][1];
 	}
 
-	public Tetrominoes getShape() { //블럭의 모양을 반환하는 메소드
+	public Tetrominoes getShape() { //블록의 모양을 반환하는 메소드
 		return pieceShape;
 	}
 
-	public void setRandomShape() { //랜덤으로 블럭의 모양을 설정하는 메소드
+	public void setRandomShape() { //랜덤으로 블록의 모양을 설정하는 메소드
 		Random r = new Random(); //랜덤 객체 생성
 		int x = Math.abs(r.nextInt()) % 7 + 1; //1부터 7까지의 랜덤한 숫자를 생성
 		Tetrominoes[] values = Tetrominoes.values(); //Tetrominoes 열거형의 모든 값들을 가져옴
 		setShape(values[x]); //랜덤한 모양을 설정
 	}
-
-	public int minX() { //블럭의 x좌표 중 가장 작은 값을 반환하는 메소드
+	
+	public int minX() { //블록의 x좌표 중 가장 작은 값을 반환하는 메소드
 		int m = coords[0][0]; //가장 작은 값으로 coords 배열의 첫 번째 칸의 x좌표를 설정
 		for (int i = 0; i < 4; i++) { //coords 배열의 모든 칸을 검사
 			m = Math.min(m, coords[i][0]); //가장 작은 값으로 m을 업데이트
@@ -67,7 +67,7 @@ public class Shape { //블럭의 모양을 나타내는 클래스
 		return m; //가장 작은 값 반환
 	}
 
-	public int minY() { //블럭의 y좌표 중 가장 작은 값을 반환하는 메소드
+	public int minY() { //블록의 y좌표 중 가장 작은 값을 반환하는 메소드
 		int m = coords[0][1];
 		for (int i = 0; i < 4; i++) {
 			m = Math.min(m, coords[i][1]);
@@ -75,21 +75,21 @@ public class Shape { //블럭의 모양을 나타내는 클래스
 		return m;
 	}
 
-	public Shape rotateLeft() { // 블럭을 왼쪽으로 회전하는 메소드
-		if (pieceShape == Tetrominoes.SquareShape) // 블럭의 모양이 SquareShape(네모)라면
-			return this; // 블럭을 회전시키지 않고 그대로 반환
+	public Shape rotateLeft() { // 블록을 왼쪽으로 회전하는 메소드
+		if (pieceShape == Tetrominoes.SquareShape) // 블록의 모양이 SquareShape(네모)라면
+			return this; // 블록을 회전시키지 않고 그대로 반환
 	
-		Shape result = new Shape(); // 새로운 블럭 result 생성
-		result.pieceShape = pieceShape; // 새로운 블럭의 모양을 현재 블럭의 모양으로 설정
+		Shape result = new Shape(); // 새로운 블록 result 생성
+		result.pieceShape = pieceShape; // 새로운 블록의 모양을 현재 블록의 모양으로 설정
 	
-		for (int i = 0; i < 4; ++i) { // 블럭의 모든 칸에 대해
+		for (int i = 0; i < 4; ++i) { // 블록의 모든 칸에 대해
 			result.setX(i, y(i)); // 현재 칸의 x 좌표를 새로운 칸의 y 좌표로 설정
 			result.setY(i, -x(i)); // 현재 칸의 y 좌표를 새로운 칸의 -x 좌표로 설정
 		}
-		return result; // 새로운 블럭 반환
+		return result; // 새로운 블록 반환
 	}
 
-	public Shape rotateRight() { //블럭을 오른쪽으로 회전하는 메소드
+	public Shape rotateRight() { //블록을 오른쪽으로 회전하는 메소드
 		if (pieceShape == Tetrominoes.SquareShape)
 			return this;
 
